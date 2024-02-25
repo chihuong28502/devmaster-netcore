374c1e3f64f1fcc07b4cef992d90d4e8eb0c8bc3
Chí Hưởng <>
2024-02-25 21:24:01 +0700

    ls 1

---
+++ MVC-netcore/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_netcore.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

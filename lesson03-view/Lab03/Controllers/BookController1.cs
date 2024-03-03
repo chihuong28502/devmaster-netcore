95abdf0faac85e444ebf54f2007f85376c216bf3
Chí Hưởng <>
2024-03-03 20:41:25 +0700

    pussh

---
+++ lesson03-view/Lab03/Controllers/BookController1.cs
@@ -0,0 +1,31 @@
+﻿using Lab03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class BookController1 : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult Index()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            var model= book.GetBookList();
+
+            return View(model);// truyền data lên view qua tham số
+        }
+        public IActionResult Create()
+        {
+            ViewBag.authors = book.Authors; 
+            ViewBag.genres = book.Genres;
+            var model = book.GetBookList();
+            return View(model);// truyền data lên view qua tham số
+        }
+        public IActionResult Edit(int id) {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            var model = book.GetBookById(id);
+            return View(model);
+        }
+    }
+}

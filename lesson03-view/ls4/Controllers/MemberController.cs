dcc9a6d947d2eed6454082dcbd9dc7cf9bac7f2b
Chí Hưởng <>
2024-03-06 07:49:35 +0700

    push

---
+++ lesson03-view/ls4/Controllers/MemberController.cs
@@ -0,0 +1,34 @@
+﻿using ls4.Models.DataModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ls4.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            var member = new Member()
+            {
+
+                MemberId = Guid.NewGuid().ToString(),
+                Username = "Chung chung",
+                FullName = "full name",
+                Password = "pass",
+                Email = "bigang02xz"
+            };
+            ViewBag.Member = member;
+            return View(member);
+        }
+        public IActionResult GetListMember() {
+            var list = new List<Member>()
+            {
+
+            };
+        return View();
+        }
+        public IActionResult Create() {
+            var model = new Member();
+            return View(model);
+        }
+    }
+}
---
+++ lesson03-view/ls4/Models/DataModels/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace ls4.Models.DataModels
+{
+    public class Member
+    {
+        public string MemberId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        
+    }
+}
